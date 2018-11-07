c752b6de2f9b781fc8ca6244b3e8fb5000633d2c
Smeet <smeetprpatel@gmail.com>
2018-11-08 08:54:04 +1300

    update

---
+++ src/NumberGuessing/Score.java
@@ -0,0 +1,16 @@
+package NumberGuessing;
+
+public class Score implements Comparable <Score>
+{
+	public String name;
+	public int score;
+	public Score(String name, int score) {
+		
+	}
+	
+	public int compareTo(Score s)
+	{
+		return 0;
+	}
+
+}
